d6485ec3929ea6cdd9aa1cab69d3f7a1039c3806
Ojala Okko <okko.ojala@hiq.fi>
2022-01-16 10:53:13 +0200

    Remove unused vars

--- config/env.ts
+++ config/env.ts
@@ -2,21 +2,19 @@ export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const GOOGLE_ANALYTICS = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 export const ENV = process.env.NEXT_PUBLIC_VERCEL_ENV;
 export const DESKTOP_BREAKPOINT = process.env.NEXT_PUBLIC_DESKTOP_BREAKPOINT;
-export const PRISMIC_API_ENDPOINT = process.env.NEXT_PUBLIC_PRISMIC_API_ENDPOINT;
+export const PRISMIC_API_ENDPOINT =
+  process.env.NEXT_PUBLIC_PRISMIC_API_ENDPOINT;
 export const PRISMIC_API_TOKEN = process.env.NEXT_PUBLIC_PRISMIC_API_TOKEN;
-export const SHOPIFY_API_STOREFRONT_TOKEN = process.env.NEXT_PUBLIC_SHOPIFY_API_STOREFRONT_TOKEN;
+export const SHOPIFY_API_STOREFRONT_TOKEN =
+  process.env.NEXT_PUBLIC_SHOPIFY_API_STOREFRONT_TOKEN;
 export const SHOPIFY_DOMAIN = process.env.NEXT_PUBLIC_SHOPIFY_DOMAIN;
 export const TITLE = process.env.NEXT_PUBLIC_TITLE;
 export const DESCRIPTION = process.env.NEXT_PUBLIC_DESCRIPTION;
-export const TWITTER_HANDLE = process.env.NEXT_PUBLIC_TWITTER_HANDLE
+export const TWITTER_HANDLE = process.env.NEXT_PUBLIC_TWITTER_HANDLE;
 export const OG_IMG = process.env.NEXT_PUBLIC_OG_IMG;
 export const INSTAGRAM_URL = process.env.NEXT_PUBLIC_INSTAGRAM_URL;
 export const INSTAGRAM_APP_ID = process.env.INSTAGRAM_APP_ID;
 export const INSTAGRAM_APP_SECRET = process.env.INSTAGRAM_APP_SECRET;
 export const INSTAGRAM_REDIRECT_URL = process.env.INSTAGRAM_REDIRECT_URL;
 export const CURRENCY = process.env.NEXT_PUBLIC_CURRENCY;
-export const REDIS_HOST = process.env.REDIS_HOST;
-export const REDIS_PORT = process.env.REDIS_PORT;
-export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 export const REDIS_URL = process.env.REDIS_URL;
-
