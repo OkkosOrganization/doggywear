a0a6c6aef45e96b800c529394b7b0bfa2904a87a
Ojala Okko <okko.ojala@hiq.fi>
2022-05-06 13:19:30 +0300

    Add text for print packaging, change file names

---
+++ components/PrintPackagingInfo.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/PrintPackagingInfo.module.scss';
+
+export const PrintPackagingInfo = () => {
+  return (
+    <div className={styles.packageInfo}>
+      <h2 className={styles.title}>Packaging</h2>
+      <p>
+        All poster items are carefully rolled into cardboard tubes and then
+        shipped out to our customers.
+      </p>
+    </div>
+  );
+};
