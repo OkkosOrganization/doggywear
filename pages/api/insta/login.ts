15d75598c0d183b9a1c3047e8a8f86165d3b112c
Okko Ojala <okko.ojala@gmail.com>
2025-01-01 17:02:57 +0200

    Change insta api url

--- pages/api/insta/login.ts
+++ pages/api/insta/login.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { INSTAGRAM_APP_ID, INSTAGRAM_REDIRECT_URL } from '../../../config/env';
 
 const loginHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const redirectUrl = `https://api.instagram.com/oauth/authorize?client_id=${INSTAGRAM_APP_ID}&redirect_uri=${INSTAGRAM_REDIRECT_URL}&scope=user_profile,user_media&response_type=code`;
+  const redirectUrl = `https://www.instagram.com/oauth/authorize?client_id=${INSTAGRAM_APP_ID}&redirect_uri=${INSTAGRAM_REDIRECT_URL}&scope=business_basic&response_type=code`;
   res.redirect(307, redirectUrl).end();
 };
 
