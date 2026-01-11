cf4d301651bd5bf5914f2b4a252c1fd14de41bfe
Okko Ojala <okko.ojala@gmail.com>
2026-01-11 20:28:09 +0200

    Remove /pages

--- pages/api/insta/login.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { INSTAGRAM_APP_ID, INSTAGRAM_REDIRECT_URL } from '../../../config/env';
-const loginHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const redirectUrl = `https://www.instagram.com/oauth/authorize?client_id=${INSTAGRAM_APP_ID}&redirect_uri=${INSTAGRAM_REDIRECT_URL}&scope=business_basic&response_type=code`;
-  res.redirect(307, redirectUrl).end();
-};
-export default loginHandler;
