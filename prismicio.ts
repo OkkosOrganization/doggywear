9d2ff79f8868de7dee52e1d1987c6ef669aea52c
Okko Ojala <okko.ojala@gmail.com>
2026-01-11 20:56:04 +0200

    Add SliceMachine

---
+++ prismicio.ts
@@ -0,0 +1,48 @@
+import * as prismic from '@prismicio/client';
+import * as prismicNext from '@prismicio/next';
+import config from './slicemachine.config.json';
+
+/**
+ * The project's Prismic repository name.
+ */
+export const repositoryName =
+  process.env.NEXT_PUBLIC_PRISMIC_ENVIRONMENT || config.repositoryName;
+
+/**
+ * A list of Route Resolver objects that define how a document's `url` field is resolved.
+ *
+ * {@link https://prismic.io/docs/route-resolver#route-resolver}
+ */
+const routes: prismic.ClientConfig['routes'] = [
+  {
+    type: 'front-page',
+    path: '/',
+  },
+  {
+    type: 'info-page',
+    path: '/info',
+  },
+  {
+    type: 'product',
+    path: '/product/:uid',
+  },
+];
+
+/**
+ * Creates a Prismic client for the project's repository. The client is used to
+ * query content from the Prismic API.
+ *
+ * @param config - Configuration for the Prismic client.
+ */
+export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
+  const client = prismic.createClient(repositoryName, {
+    routes,
+    fetchOptions: {
+      cache: process.env.NODE_ENV === 'production' ? 'force-cache' : 'no-store',
+      next: { tags: ['prismic'] },
+    },
+    ...config,
+  });
+
+  return client;
+};
