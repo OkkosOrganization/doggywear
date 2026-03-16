e64427df4bc780a889c06b86d0915678499d1d6f
Okko Ojala <okko.ojala@gmail.com>
2026-03-16 13:51:58 +0200

    Refactor

--- components/RelatedProducts.tsx
+++ components/RelatedProducts.tsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from 'react';
 import { ProductCard } from './ProductCard';
 import styles from '../styles/RelatedProducts.module.css';
 import scrollerStyles from '../styles/SharedScroller.module.css';
+import { ProductDocument } from '../prismicio-types';
 
 type RelatedProductsProps = {
-  products: any[];
+  products: ProductDocument<string>[];
   filterByTag?: string;
-  isMobile: boolean;
   randomOrder?: boolean;
 };
 export const RelatedProducts = ({
@@ -14,40 +14,38 @@ export const RelatedProducts = ({
   filterByTag,
   randomOrder,
 }: RelatedProductsProps) => {
-  const [randomized, setRandomized] = useState<any[]>(products);
+  const [randomized, setRandomized] =
+    useState<ProductDocument<string>[]>(products);
 
   useEffect(() => {
     if (randomOrder)
       setRandomized(products.sort((a, b) => 0.5 - Math.random()));
   }, [randomOrder, products]);
 
-  if (!products) return null;
-  const items = randomized.map((p, pindex) => {
-    return (
-      <div
-        className={scrollerStyles.scrollerItem}
-        key={'RelatedProduct_' + p.uid}
-      >
-        <div className={scrollerStyles.scrollerItemInner}>
-          <ProductCard
-            data={p}
-            showMultipleImages={false}
-            loadImagesEager={false}
-            sizes={'(max-width: 768px) 100vw, 25vw'}
-          />
-        </div>
-      </div>
-    );
-  });
+  if (!products || products.length === 0) return null;
 
   return (
     <div className={styles.relatedProducts}>
-      {items.length ? (
-        <>
-          <h3 className={styles.title}>You might also like</h3>
-          <div className={scrollerStyles.scroller}>{items}</div>
-        </>
-      ) : null}
+      <h3 className={styles.title}>You might also like</h3>
+      <div className={scrollerStyles.scroller}>
+        {randomized.map((p) => {
+          return (
+            <div
+              className={scrollerStyles.scrollerItem}
+              key={'RelatedProduct_' + p.uid}
+            >
+              <div className={scrollerStyles.scrollerItemInner}>
+                <ProductCard
+                  data={p}
+                  showMultipleImages={false}
+                  loadImagesEager={false}
+                  sizes={'(max-width: 768px) 100vw, 25vw'}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
